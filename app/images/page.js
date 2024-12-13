cfe6e9d2ae32c686904d5ed22601a45f04605245
David Edin <ed.david.edin@gmail.com>
2024-12-14 00:49:04 +0100

    Restart

--- app/images/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import GetImages from "./getImages.js";
-
-import "./images.css";
-
-// Display photos
-export default function imagesPage() {
-  return <GetImages />;
-}
